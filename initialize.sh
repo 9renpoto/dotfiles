a7ed026ff77f41910775284053d910c36710793f
Kan Keisuke <9renpoto@gmail.com>
2013-07-08 20:43:15 +0900

    add _kerlrc

--- initialize.sh
+++ initialize.sh
@@ -7,3 +7,6 @@ ln -s ~/dotfiles/_tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/_zshrc ~/.zshrc
 
 ln -s ~/dotfiles/_vimrc ~/.vimrc
+
+# http://qiita.com/tatsuya6502@github/items/a356db103f3b654d6f27
+# ln -s ~/dotfiles/_kerlrc ~/.kerlrc
