daf99a77d7d4bbc5c339db8ce733ef8d83dab674
Kan Keisuke <9renpoto@gmail.com>
2013-11-22 10:21:31 +0900

    add mkdir

--- initialize.sh
+++ initialize.sh
@@ -3,6 +3,10 @@
 cd ~/
 
 mkdir ~/vimbackup
+mkdir ~/github
+mkdir ~/bitbucket
+mkdir ~/.zsh
+git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 
 ln -s ~/dotfiles/_bashrc ~/.bashrc
 ln -s ~/dotfiles/_tmux.conf ~/.tmux.conf
@@ -12,3 +16,4 @@ ln -s ~/dotfiles/_vimrc ~/.vimrc
 
 # http://qiita.com/tatsuya6502@github/items/a356db103f3b654d6f27
 # ln -s ~/dotfiles/_kerlrc ~/.kerlrc
+
