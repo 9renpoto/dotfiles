eef45192a1abd627862127ffe7f8e083f60246f2
Kan Keisuke <9renpoto@gmail.com>
2013-11-27 15:13:18 +0900

    add tmux-powerline

--- initialize.sh
+++ initialize.sh
@@ -12,13 +12,16 @@ git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-synt
 mkdir -p ~/.vim/bundle
 git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
+mkdir ~/.tmux
+git clone git://github.com/erikw/tmux-powerline.git ~/.tmux/tmux-powerline
+
 ln -s ~/dotfiles/_bashrc ~/.bashrc
 ln -s ~/dotfiles/_tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/_zshrc ~/.zshrc
-ln -s ~/dotfiles/_zsh ~/.zsh
 
 ln -s ~/dotfiles/_vimrc ~/.vimrc
 
 # http://qiita.com/tatsuya6502@github/items/a356db103f3b654d6f27
 # ln -s ~/dotfiles/_kerlrc ~/.kerlrc
+
 
