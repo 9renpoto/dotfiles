7dae9b06c17780a578698c9a08cde23f4da668aa
Kan Keisuke <9renpoto@gmail.com>
2014-05-31 16:22:24 +0900

    update tmux setting

--- initialize.sh
+++ initialize.sh
@@ -5,9 +5,7 @@ cd ~/
 mkdir -p ~/vimbackup
 mkdir -p ~/github
 mkdir -p ~/bitbucket
-mkdir -p ~/.vim
 mkdir -p ~/.tmux
-mkdir -p ~/.zsh
 
 ln -s ~/github/dotfiles/_bash_profile ~/.bash_profile
 ln -s ~/github/dotfiles/_bashrc ~/.bashrc
@@ -18,6 +16,7 @@ ln -s ~/github/dotfiles/_vimrc ~/.vimrc
 
 ln -s ~/github/dotfiles/vim ~/.vim
 ln -s ~/github/dotfiles/zsh ~/.zsh
+ln -s ~/github/dotfiles/tmux ~/.tmux
 
 # http://qiita.com/tatsuya6502@github/items/a356db103f3b654d6f27
 # ln -s ~/dotfiles/_kerlrc ~/.kerlrc
