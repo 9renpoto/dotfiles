fdabb8b9acd9c02f9b6c8537cc6a18ce074b938e
Kan Keisuke <9renpoto@gmail.com>
2014-05-31 15:44:10 +0900

    upate path

--- initialize.sh
+++ initialize.sh
@@ -10,18 +10,17 @@ mkdir ~/.zsh
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 
 mkdir -p ~/.vim/bundle
-git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
+# git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
 mkdir ~/.tmux
-git clone git://github.com/erikw/tmux-powerline.git ~/.tmux/tmux-powerline
+# git clone git://github.com/erikw/tmux-powerline.git ~/.tmux/tmux-powerline
 
-ln -s ~/dotfiles/_bash_profile ~/.bash_profile
-ln -s ~/dotfiles/_bashrc ~/.bashrc
-ln -s ~/dotfiles/_tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/_zshenv ~/.zshenv
-ln -s ~/dotfiles/_zshrc ~/.zshrc
-
-ln -s ~/dotfiles/_vimrc ~/.vimrc
+ln -s ~/github/dotfiles/_bash_profile ~/.bash_profile
+ln -s ~/github/dotfiles/_bashrc ~/.bashrc
+ln -s ~/github/dotfiles/_tmux.conf ~/.tmux.conf
+ln -s ~/github/dotfiles/_zshenv ~/.zshenv
+ln -s ~/github/dotfiles/_zshrc ~/.zshrc
+ln -s ~/github/dotfiles/_vimrc ~/.vimrc
 
 # http://qiita.com/tatsuya6502@github/items/a356db103f3b654d6f27
 # ln -s ~/dotfiles/_kerlrc ~/.kerlrc
