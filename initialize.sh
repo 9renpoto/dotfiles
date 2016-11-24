d394e71cb448975493df1ad298067a6852b8425c
Keisuke Kan <9renpoto@gmail.com>
2016-11-24 23:57:58 +0900

    feat(.asdf) add asdf

--- initialize.sh
+++ initialize.sh
@@ -1,26 +1,20 @@
-#!/user/bin/sh
+#!/bin/sh
 
 # http://qiita.com/usamik26/items/601f5612bd3f8a21cc41
 cd /opt
-sudo mkdir homebrew
+sudo mkdir -p homebrew
 sudo chown ${USER}:staff homebrew
 
 mkdir -p ~/vimbackup
 
+ln -s ~/src/github.com/9renpoto/dotfiles/.agignore ~/.agignore
+ln -s ~/src/github.com/9renpoto/dotfiles/.asdf ~/.asdf
 ln -s ~/src/github.com/9renpoto/dotfiles/.editorconfig ~/.editorconfig
+ln -s ~/src/github.com/9renpoto/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/src/github.com/9renpoto/dotfiles/.hyper.js ~/.hyper.js
+ln -s ~/src/github.com/9renpoto/dotfiles/.ignore ~/.gitignore
 ln -s ~/src/github.com/9renpoto/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/src/github.com/9renpoto/dotfiles/.vimrc ~/.vimrc
 ln -s ~/src/github.com/9renpoto/dotfiles/.zshenv ~/.zshenv
 ln -s ~/src/github.com/9renpoto/dotfiles/.zshrc ~/.zshrc
-ln -s ~/src/github.com/9renpoto/dotfiles/.vimrc ~/.vimrc
-
-ln -s ~/src/github.com/9renpoto/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/src/github.com/9renpoto/dotfiles/.ignore ~/.gitignore
-ln -s ~/src/github.com/9renpoto/dotfiles/.agignore ~/.agignore
-
 ln -s ~/src/github.com/9renpoto/dotfiles/vim ~/.vim
-ln -s ~/src/github.com/9renpoto/dotfiles/.tmux.conf ~/.tmux.conf
-
-ln -s ~/src/github.com/9renpoto/dotfiles/.hyper.js ~/.hyper.js
-
-# http://qiita.com/tatsuya6502@github/items/a356db103f3b654d6f27
-# ln -s ~/src/github.com/9renpoto/dotfiles/.kerlrc ~/.kerlrc
