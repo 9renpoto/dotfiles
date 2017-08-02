fa7f0128b79a5d6b551bd1532ba596aecda3e3c4
Keisuke Kan <9renpoto@gmail.com>
2017-08-03 00:25:46 +0900

    fix(.config/fish) fix pyenv path (#101)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,9 @@ set -x HOSTNAME hostname
 
 set -x PATH /opt/homebrew/bin $PATH
 
+set -x PATH $HOME/.pyenv/bin $PATH
+. (pyenv init - | psub)
+
 if test -e $HOME/src/bitbucket.org/9renpoto/ssh/.keychain
   . $HOME/src/bitbucket.org/9renpoto/ssh/.keychain
 end
