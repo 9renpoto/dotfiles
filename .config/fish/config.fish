0d94c3e2ee2bdeb964f660878edc752d9a83ae01
Keisuke Kan <9renpoto@gmail.com>
2017-10-21 00:09:07 +0900

    refactor(.config/fish) add alias vim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,8 @@ set -x HOSTNAME hostname
 set -x PATH /opt/homebrew/bin $PATH
 set -gx LC_ALL en_US.UTF-8
 
+alias vim='/opt/homebrew/bin/nvim'
+
 if test -e $HOME/src/bitbucket.org/9renpoto/ssh/.keychain
   . $HOME/src/bitbucket.org/9renpoto/ssh/.keychain
 end
