2c234d14a9b61edd15298723dc641f3f864fbf2d
Keisuke Kan <9renpoto@gmail.com>
2020-12-13 11:36:14 +0900

    fix: plugins

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,15 +2,13 @@ set -x XDG_CONFIG_HOME $HOME/.config
 set -x EDITOR nvim
 set -x HOSTNAME hostname
 set -x GOPATH $HOME
+set -g GHQ_SELECTOR_OPTS "--no-sort --reverse --ansi --color bg+:13,hl:3,pointer:7"
+set -U FZF_LEGACY_KEYBINDINGS 0
 
-set -x PATH /usr/local/bin $PATH
 set -x PATH ~/.deno/bin $PATH
 set -gx LC_ALL en_US.UTF-8
 
 alias vim='/usr/local/bin/nvim'
 
-if test -e $HOME/src/github.com/9renpoto/ssh/.keychain
-  . $HOME/src/github.com/9renpoto/ssh/.keychain
-end
 source (brew --prefix asdf)/asdf.fish
 eval (starship init fish)
