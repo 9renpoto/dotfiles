664a30ec02636f707d399805b737720391aad529
Keisuke Kan <9renpoto@gmail.com>
2020-12-13 11:47:44 +0900

    fix: gcloud path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,6 @@ set -x XDG_CONFIG_HOME $HOME/.config
 set -x EDITOR nvim
 set -x HOSTNAME hostname
 set -x GOPATH $HOME
-set -g GHQ_SELECTOR_OPTS "--no-sort --reverse --ansi --color bg+:13,hl:3,pointer:7"
 set -U FZF_LEGACY_KEYBINDINGS 0
 
 set -x PATH ~/.deno/bin $PATH
@@ -10,5 +9,6 @@ set -gx LC_ALL en_US.UTF-8
 
 alias vim='/usr/local/bin/nvim'
 
+source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
 source (brew --prefix asdf)/asdf.fish
 eval (starship init fish)
