6b9622ef0d9d75a50fe68e52c462fc43abb5e8a4
Keisuke Umeno <9renpoto@gmail.com>
2022-09-27 22:37:00 +0900

    fix: remove hard code

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,8 +7,5 @@ set -U FZF_LEGACY_KEYBINDINGS 0
 set -x PATH ~/.deno/bin $PATH
 set -gx LC_ALL en_US.UTF-8
 
-alias vim='/usr/local/bin/nvim'
-
-source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
 source (brew --prefix asdf)/asdf.fish
 eval (starship init fish)
