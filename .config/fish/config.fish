c81fcba07fe60581f76a48cffc2ed510485c4835
Keisuke Umeno <9renpoto@gmail.com>
2022-12-23 16:19:47 +0900

    chore: refine fish plugins

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,10 +2,8 @@ set -x XDG_CONFIG_HOME $HOME/.config
 set -x EDITOR nvim
 set -x HOSTNAME hostname
 set -x GOPATH $HOME
-set -U FZF_LEGACY_KEYBINDINGS 0
 
 set -x PATH ~/.deno/bin $PATH
 set -gx LC_ALL en_US.UTF-8
 
-source (brew --prefix asdf)/asdf.fish
 eval (starship init fish)
